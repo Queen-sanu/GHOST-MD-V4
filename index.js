686344e6cc48b9acd9348aaa3df69e35d88994e3
Queen-sanu <prarthanashashini93@gmail.com>
2025-06-14 19:22:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ var { updateCMDStore,isbtnID,getCMDStore,getCmdForCmdId,connectdb,input,get,updb
 //===================SESSION============================
 if (!fs.existsSync(__dirname + '/auth_info_baileys/creds.json')) {
   if (config.SESSION_ID) {
-  const sessdata = config.SESSION_ID.replace("GHOSTMD=","")
+  const sessdata = config.SESSION_ID.replace("SANU-MD=")
   const filer = File.fromURL(`https://mega.nz/file/${sessdata}`)
   filer.download((err, data) => {
     if (err) throw err
